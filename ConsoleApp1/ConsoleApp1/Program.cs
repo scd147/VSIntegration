ea57ea3cbd74e70cf040e5d15da0f5d6076213ec
Steven Donahue <scd147@live.com>
2020-07-07 11:02:33 -0400

    Add Day of Week initial logic

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,21 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Mod in GitHub
-            Console.WriteLine("Hello World!");
+            DateTime dConvert = DateTime.Now;
+            
+            Console.WriteLine(dConvert);
+
+            string sInstruct = "";
+            sInstruct = Console.ReadLine();
+
+            if (sInstruct.ToUpper() == "DOW" || sInstruct.ToUpper() == "DAY OF WEEK")
+                {
+                Console.WriteLine(dConvert.DayOfWeek);
+            }
+            else
+                {
+                Console.WriteLine("You probably screwed something up.  It's your fault.");
+            }
             //Mod in VS, but how to compare?
         }
     }
